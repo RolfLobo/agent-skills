aee02856714dddad8d279631164632ca9e449fc8
Felipe Rodrigues <me@felipefr.dev>
2026-01-25 16:14:05 -0300

    feat: add moduleNameMapper for core library path resolution

--- tools/skill-plugin/jest.config.ts
+++ tools/skill-plugin/jest.config.ts
@@ -5,4 +5,7 @@ export default {
   transform: { '^.+\\.[tj]s$': ['ts-jest', { useESM: true, tsconfig: '<rootDir>/tsconfig.spec.json' }] },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/tools/skill-plugin',
+  moduleNameMapper: {
+    '^@tech-leads-club/core$': '<rootDir>/../../libs/core/src/index.ts',
+  },
 }
